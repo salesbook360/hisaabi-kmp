c9f7343095bfb2b33a05c5e73965bcb0fe968044
Ali Sabir <>
2025-12-18 20:55:54 +0500

    JVM build issue fixed.

--- composeApp/src/jvmMain/kotlin/com/hisaabi/hisaabi_kmp/di/PlatformModule.jvm.kt
+++ composeApp/src/jvmMain/kotlin/com/hisaabi/hisaabi_kmp/di/PlatformModule.jvm.kt
@@ -5,5 +5,9 @@ import org.koin.dsl.module
 
 actual fun platformModule() = module {
     single { DatabaseBuilder() }
+    
+    // Reports platform-specific dependencies
+    single { com.hisaabi.hisaabi_kmp.reports.domain.util.ReportPdfGenerator() }
+    single { com.hisaabi.hisaabi_kmp.reports.domain.util.ShareHelper() }
 }
 
